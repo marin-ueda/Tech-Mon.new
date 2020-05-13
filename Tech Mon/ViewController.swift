6423ef1c871400b14eb909c83e2a487d650fc2f0
Marin Ueda <marin96nym@gmail.com>
2020-05-14 01:39:15 +0900

    Initial Commit

---
+++ Tech Mon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tech Mon
+//
+//  Created by 植田真梨 on 2020/05/14.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
