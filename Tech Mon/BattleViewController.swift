cf3253242b342b36e0623c1ce38cfad3f0a8679f
Marin Ueda <marin96nym@gmail.com>
2020-05-15 05:50:45 +0900

    一通り。改善点あり

--- Tech Mon/BattleViewController.swift
+++ Tech Mon/BattleViewController.swift
@@ -14,6 +14,7 @@ class BattleViewController: UIViewController {
     @IBOutlet var playerImageView: UIImageView!
     @IBOutlet var playerHPLabel: UILabel!
     @IBOutlet var playerMPLabel: UILabel!
+    @IBOutlet var playerTPLabel: UILabel!
     
     @IBOutlet var enemyNameLabel: UILabel!
     @IBOutlet var enemyImageView: UIImageView!
@@ -22,13 +23,18 @@ class BattleViewController: UIViewController {
     
     let techMonManager = TechMonManager.shared
     
-
+    var player: Character!
+    var enemy: Character!
     var gameTimer: Timer!
     var isPlayerAttackAvailable: Bool = true
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        //キャラクターの読み込み
+        player = techMonManager.player
+        enemy = techMonManager.enemy
         
         //プレイヤーのステータスを反映
         playerNameLabel.text = "勇者"
@@ -58,7 +64,17 @@ class BattleViewController: UIViewController {
         super.viewWillDisappear(animated)
         techMonManager.stopBGM()
     }
-    
+    //ステータスの反映
+    func updateUI() {
+        
+        //プレーヤーのステータスを反映
+        playerHPLabel.text = "\(player.currentHP) / \(player.maxHP)"
+        playerMPLabel.text = "\(player.currentMP) / \(player.maxMP)"
+        playerTPLabel.text = "\(player.currentTP) / \(player.maxTP)"
+        //敵のステータスを反映
+        enemyHPLabel.text = "\(enemy.currentHP) / \(enemy.maxHP)"
+        enemyMPLabel.text = "\(enemy.currentMP) / \(enemy.maxMP)"
+    }
     //0.1秒ごとにゲームの状態を更新する
     func updateGame() {
         
@@ -101,6 +117,18 @@ class BattleViewController: UIViewController {
         }
     }
     
+    //勝敗判定をする
+    func judgeBattle() {
+        
+        if player.currentHP <= 0 {
+            
+            finishBattle(vanishImageView: playerImageView, isPlayerWin: false)
+        } else if enemy.currentHP <= 0 {
+            
+            finishBattle(vanishImageView: enemyImageView, isPlayerWin: true)
+        }
+    }
+    
     //勝利が決定した後の処理
     func finishBattle(vanishImageView: UIImageView, isPlayerWin: Bool) {
         
@@ -135,16 +163,54 @@ class BattleViewController: UIViewController {
             techMonManager.damageAnimation(imageView: enemyImageView)
             techMonManager.playSE(fileName: "SE_attack")
             
-            enemyHP -= 30
-            playerMP = 0
+            enemy.currentHP -= player.attackPoint
             
-            enemyHPLabel.text = "\(enemyHP) / 200"
+            player.currentTP += 10
+            if player.currentTP >= player.maxTP {
+                player.currentTP = player.maxTP
+            }
+            //enemyHP -= 30
+            player.currentMP = 0
+            
+           /* enemyHPLabel.text = "\(enemyHP) / 200"
             playerMPLabel.text = "\(playerMP) / 20"
             
             if enemyHP <= 0 {
                 
-                finishBattle(vanishImageView: enemyImageView, isPlayerWin: true)
+                finishBattle(vanishImageView: enemyImageView, isPlayerWin: true)*/
+            judgeBattle()
             }
+        }
+    @IBAction func fireAction() {
+        if isPlayerAttackAvailable && player.currentTP >= 40 {
+            
+            techMonManager.damageAnimation(imageView: enemyImageView)
+            techMonManager.playSE(fileName: "SE_fire")
+            
+            enemy.currentHP -= 100
+            
+            player.currentTP -= 40
+            if player.currentTP <= 0 {
+                
+                player.currentTP = 0
+            }
+            player.currentMP = 0
+            
+            judgeBattle()
+        }
+    }
+    
+    @IBAction func tameruAction() {
+        
+        if isPlayerAttackAvailable {
+            
+            techMonManager.playSE(fileName: "SE_charge")
+            player.currentTP += 40
+            if player.currentTP >= player.maxTP {
+                
+                player.currentTP = player.maxTP
+            }
+            player.currentMP = 0
         }
     }
 
